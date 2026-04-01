0d2aada567294383b48eb58185cd225a529e9177
Edmund Lodewijks <edmund@proteamail.com>
2026-04-01 16:42:22 +0200

    FIX: Change set-version sed commands so they work on BSD. Change VERSION to 4.0.0 for the new release.

--- tools/set-version.sh
+++ tools/set-version.sh
@@ -29,17 +29,17 @@ VERSION="$(cat VERSION)"
 TODAY="$(date +%Y-%m-%d)"
 
 # postallow script
-sed -i \
+sed \
     -e "s/^version=.*/version=\"${VERSION}\"/" \
     -e "s/^lastupdated=.*/lastupdated=\"${TODAY}\"/" \
-    postallow
+    postallow > postallow.tmp && mv postallow.tmp postallow
 
 # man pages: replace the date and version in the .TH line
 # .TH POSTALLOW 1 "date" "Postallow version" "section"
 for page in man/man1/postallow.1 man/man5/postallow.conf.5; do
-    sed -i \
+    sed \
         -e "s/^\(\.TH [A-Z.]* [0-9] \)\"[^\"]*\" \"Postallow [^\"]*\"/\1\"${TODAY}\" \"Postallow ${VERSION}\"/" \
-        "${page}"
+        "${page}" > "${page}.tmp" && mv "${page}.tmp" "${page}"
 done
 
 printf 'Version set to %s (date %s)\n' "${VERSION}" "${TODAY}"
